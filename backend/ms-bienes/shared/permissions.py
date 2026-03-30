6702009b7629ac7952bc9d7d94c83b9233cbe4ea
ChristFlores <christcobain@gmail.com>
2026-03-30 13:17:40 -0500

    config .env y setting v2s

--- backend/ms-bienes/shared/permissions.py
+++ backend/ms-bienes/shared/permissions.py
@@ -8,24 +8,24 @@ class IsSysAdmin(BasePermission):
         return getattr(request.auth, 'get', lambda k, d=None: d)('role') == 'SYSADMIN'
 
 class HasJWTPermission(BasePermission):
-    # message = "No tiene permisos para realizar esta acción."
-    # def __init__(self, codename: str):
-    #     self.codename = codename
+    message = "No tiene permisos para realizar esta acción."
+    def __init__(self, codename: str):
+        self.codename = codename
     # def has_permission(self, request, view):
     #     if not request.user or not request.user.is_authenticated:
     #         return False
     #     if request.auth.get('role') == 'SYSADMIN':
     #         return True
     #     return self.codename in set(request.auth.get('permissions_flat', []))   
-    message = "No tiene permisos para realizar esta acción."
-    codename: str = ''
-    @classmethod
-    def require(cls, codename: str) -> type:
-        return type(
-            f'HasJWTPermission_{codename}',
-            (cls,),
-            {'codename': codename},
-        ) 
+    # message = "No tiene permisos para realizar esta acción."
+    # codename: str = ''
+    # @classmethod
+    # def require(cls, codename: str) -> type:
+    #     return type(
+    #         f'HasJWTPermission_{codename}',
+    #         (cls,),
+    #         {'codename': codename},
+    #     ) 
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
             return False
