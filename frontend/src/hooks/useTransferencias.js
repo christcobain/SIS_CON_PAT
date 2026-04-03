ba71bffc4ac6714be4f03f4e98cef740a1f10709
ChristFlores <christcobain@gmail.com>
2026-04-03 14:50:08 -0500

    actuaizacion de transfv11

--- frontend/src/hooks/useTransferencias.js
+++ frontend/src/hooks/useTransferencias.js
@@ -24,7 +24,7 @@ export function useTransferencias(activeTab, params) {
     } finally {
       setLoading(false);
     }
-  }, [activeTab, params]);
+  }, [activeTab]);
  
   useEffect(() => {
     fetchTransferencias();
