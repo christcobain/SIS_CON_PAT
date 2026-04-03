b68e773753051bc86cc8832b2afec0a8551a906a
ChristFlores <christcobain@gmail.com>
2026-04-03 00:29:23 -0500

    problema de logiout v14

--- backend/ms-usuarios/authentication/backends.py
+++ backend/ms-usuarios/authentication/backends.py
@@ -2,10 +2,45 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
 from django.conf import settings as django_settings
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
+from django.conf import settings
+from rest_framework_simplejwt.models import TokenUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+# class CookieJWTAuthentication(JWTAuthentication):
+#     def authenticate(self, request):
+#         cookie_name = getattr(django_settings, 'JWT_AUTH_COOKIE', 'sisconpat_access')
+#         raw_token = request.COOKIES.get(cookie_name)
+#         if raw_token is None:
+#             return None
+#         try:
+#             validated_token = self.get_validated_token(raw_token)
+#         except InvalidToken:
+#             return None
+#         return self.get_user(validated_token), validated_token
+
+
+
+
+
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        cookie_name = getattr(django_settings, 'JWT_AUTH_COOKIE', 'sisconpat_access')
-        raw_token = request.COOKIES.get(cookie_name)
+        header = self.get_header(request)
+        if header is not None:
+            raw_token = self.get_raw_token(header)
+            if raw_token is not None:
+                try:
+                    validated_token = self.get_validated_token(raw_token)
+                    return self.get_user(validated_token), validated_token
+                except InvalidToken:
+                    return None
+        raw_token = request.COOKIES.get(
+            getattr(settings, 'JWT_AUTH_COOKIE', 'sisconpat_access')
+        )
         if raw_token is None:
             return None
         try:
@@ -13,3 +48,44 @@ class CookieJWTAuthentication(JWTAuthentication):
         except InvalidToken:
             return None
         return self.get_user(validated_token), validated_token
+
+    def get_user(self, validated_token):
+        try:
+            user_id = validated_token['user_id']
+            return User.objects.get(pk=user_id)
+        except (KeyError, User.DoesNotExist):
+            return None
+class CookieOnlyJWTAuthentication(CookieJWTAuthentication):
+    def authenticate(self, request):
+        raw_token = request.COOKIES.get(
+            getattr(settings, 'JWT_AUTH_COOKIE', 'sisconpat_access')
+        )
+        if raw_token is None:
+            return None
+        try:
+            validated_token = self.get_validated_token(raw_token)
+        except InvalidToken:
+            return None
+        return self.get_user(validated_token), validated_token
+class AdminJWTAuthBackend:
+    def authenticate(self, request, **kwargs):
+        if not request:
+            return None
+        raw_token = request.COOKIES.get(
+            getattr(settings, 'JWT_AUTH_COOKIE', 'sisconpat_access')
+        )
+        if not raw_token:
+            return None
+        try:
+            auth = JWTAuthentication()
+            validated_token = auth.get_validated_token(raw_token)
+            if validated_token.get('role') == 'SYSADMIN':
+                return User.objects.filter(username='SYSADMIN', is_staff=True).first()
+        except Exception:
+            return None
+        return None
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
